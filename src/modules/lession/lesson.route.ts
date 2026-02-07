35235037c580c6858887987ced5c9a6030ae6849
Mojtaba Ahmadzadeh <mojtabaahmadzadeh.2024@gmail.com>
2026-02-07 21:38:47 +0330

    feat(lesson): add validation middleware for lessons

--- src/modules/lession/lesson.route.ts
+++ src/modules/lession/lesson.route.ts
@@ -1,12 +1,40 @@
 import { Router } from "express";
 import lessionController from "./lesson.controller";
+import { rbacGuard } from "../../middleware/guard/rbac.guard";
+import { Permissions } from "../../constant/role.constant";
+import {
+  validateCreateLesson,
+  validateDeleteLesson,
+  validateGetLesson,
+  validateUpdateLesson,
+} from "./lesson.validation";
 
-const lessionRouter = Router()
+const lessionRouter = Router();
 
-lessionRouter.post('/create', lessionController.createLesson)
-lessionRouter.get('/', lessionController.getAllLesson)
-lessionRouter.get('/:id', lessionController.getByIdLesson)
-lessionRouter.put('/update/:id', lessionController.updateLesson)
-lessionRouter.delete("/delete/:id", lessionController.deleteLesson);
+lessionRouter.post(
+  "/create",
+  rbacGuard([Permissions.LESSON_CREATE]),
+  validateCreateLesson,
+  lessionController.createLesson,
+);
+lessionRouter.get("/", lessionController.getAllLesson);
+lessionRouter.get(
+  "/:id",
+  rbacGuard([Permissions.LESSON_BY_ID]),
+  validateGetLesson,
+  lessionController.getByIdLesson,
+);
+lessionRouter.put(
+  "/update/:id",
+  rbacGuard([Permissions.LESSON_UPDATE]),
+  validateUpdateLesson,
+  lessionController.updateLesson,
+);
+lessionRouter.delete(
+  "/delete/:id",
+  rbacGuard([Permissions.LESSON_DELETE]),
+  validateDeleteLesson,
+  lessionController.deleteLesson,
+);
 
-export default lessionRouter
+export default lessionRouter;
